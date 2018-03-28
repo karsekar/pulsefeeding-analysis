694c5327202adafc5693f7b6a2dbfcd5b3749914
Karthik Sekar <karsekar@gmail.com>
2018-03-28 09:44:48 +0200

    Updated with quantification of cell extension rate and protein synthesis rate

--- 10microfluidics/proteinandsize.m
+++ 10microfluidics/proteinandsize.m
@@ -1,7 +1,7 @@
 %look at size and protein synthesis of non-dividing and dividing cells from
 %microfluidic measurements
 % by Karthik Sekar (karsekar@gmail.com) with preprocessing done by Roberto Rusconi
-% last updated 24.10.2017
+% last updated 26.03.2018
 
 clear all;
 close all;
@@ -13,8 +13,13 @@ nondiv_protein = readtable('protein_and_size_data/nondiv_protein.csv');
 div_length = readtable('protein_and_size_data/div_length.csv');
 nondiv_length = readtable('protein_and_size_data/nondiv_length.csv');
 
+nondividetime = 90; %set a time for where the cells don't divide before
+
+
 %indicate the time range
 range = div_protein.time_min_';
+ofinterest =  find(range < nondividetime);
+
 
 %first make the protein synthesis figure
 
@@ -22,6 +27,29 @@ range = div_protein.time_min_';
 %non-dividing cells
 patchrange = [range flip(range)];
 patchys = horzcat([std(nondiv_protein{:,2:end},0,2)/sqrt(5) + mean(nondiv_protein{:,2:end},2)]',flip([mean(nondiv_protein{:,2:end},2) - std(nondiv_protein{:,2:end},0,2)/sqrt(5)]'));
+
+%calculate summary data 
+
+pr_slopes = zeros(1,length(nondiv_protein{1,2:end}));
+
+for i=1:length(nondiv_protein{1,2:end})
+    P =  polyfit(range(ofinterest)', nondiv_protein{ofinterest,i+1},1);
+    pr_slopes(i) =  P(1);
+end
+
+nondiv_mean = mean(pr_slopes);
+nondiv_ste =  std(pr_slopes)/sqrt(length(pr_slopes));
+
+pr_slopes = zeros(1,length(nondiv_protein{1,2:end}));
+
+for i=1:length(div_protein{1,2:end})
+    P =  polyfit(range(ofinterest)', div_protein{ofinterest,i+1},1);
+    pr_slopes(i) =  P(1);
+end
+
+div_mean = mean(pr_slopes);
+div_ste =  std(pr_slopes)/sqrt(length(pr_slopes));
+
 patch(patchrange,patchys,[250/255 219/255 206/255],'EdgeColor','none');
 hold on;
 
@@ -38,23 +66,41 @@ xlabel('Time from start of feeding (min)');
 ylabel('Normalized GFP activity');
 legend(h,'Dividing cells','Non-dividing cells');
 
-%make the cell size figures;
+figure;
+
+bar([div_mean  nondiv_mean]);
+hold on;
+errorbar([div_mean  nondiv_mean],[div_ste nondiv_ste],'.');
+
+%make the cell size figures and calculate the extension rate
 
 %first the dividing cells
 figure;
 hold on;
 %drange = length(Dmean_obj(:,1));
+
+slopes = zeros(1,length(div_length{1,2:end}));
+
 drange = div_length.time_min_;
+ofinterest =  find(drange < nondividetime);
+
 for i=1:length(div_length{1,2:end})
     plot(drange,div_length{:,i+1},'Color',[0.7 0.7 0.7])
+    P = polyfit(drange(ofinterest), div_length{ofinterest,i+1}, 1); %perform a linear regression on the time before division
+    slopes(i) =  P(1);
 end
+
+avgslope = mean(slopes);
+stderr =  std(slopes)/sqrt(length(div_length{1,2:end}));
 
 plot(drange,mean(div_length{:,2:end},2),'LineWidth',2,'Color',[0/255 114/255 189/255])
 
 ylim([1.5 4.5]);
 xlabel('Time from start of feeding (min)');
-ylabel('Cell length (um)');
+ylabel('Cell length (\mum)');
 title('Dividing cells');
+
+text(5,4.2,{'Average extension rate before 90 min: ',[num2str(avgslope,2) ' \mum/min \pm ' num2str(stderr,2) ' \mum/min']});
 
 
 %then the non-dividing cells
@@ -62,13 +108,21 @@ figure;
 hold on;
 for i=1:length(nondiv_length{1,2:end})
     plot(drange,nondiv_length{:,i+1},'Color',[0.7 0.7 0.7])
+    P = polyfit(drange(ofinterest), nondiv_length{ofinterest,i+1}, 1); %perform a linear regression on the time before division
+    slopes(i) =  P(1);
 end
+
+avgslope = mean(slopes);
+stderr =  std(slopes)/sqrt(length(div_length{1,2:end}));
 
 plot(drange,mean(nondiv_length{:,2:end},2),'LineWidth',2,'Color',[217/255 83/255 25/255])
 ylim([1.5 4.5]);
 xlabel('Time from start of feeding (min)');
-ylabel('Cell length (um)');
+ylabel('Cell length (\mum)');
 
 title('Non-dividing cells');
+
+text(5,4.2,{'Average extension rate before 90 min: ',[num2str(avgslope,2) ' \mum/min \pm ' num2str(stderr,2) ' \mum/min']});
+
 
 
